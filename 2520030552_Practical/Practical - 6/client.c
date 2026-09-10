910aa945c8c00d0aceabbc7077da2d16fc3c1de4
GNANESHWAR <pappulagnaneshwarreddy@gmail.com>
2026-09-10 11:20:45 +0530

    Add client program for FIFO communication
    This file implements a client that sends a message to a FIFO and reads a response from another FIFO.

---
+++ 2520030552_Practical/Practical - 6/client.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    char message[100];
+    char response[100];
+
+    int fd = open("myfifo", O_WRONLY);
+
+    printf("Enter message: ");
+    fgets(message, sizeof(message), stdin);
+
+    write(fd, message, sizeof(message));
+
+    close(fd);
+
+    int fd2 = open("responsefifo", O_RDONLY);
+
+    read(fd2, response, sizeof(response));
+
+    printf("Server: %s\n", response);
+
+    close(fd2);
+
+    return 0;
+}
