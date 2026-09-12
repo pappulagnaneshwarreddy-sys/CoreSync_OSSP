fcf97b342fb77561b1113fb18cc04805e84dea9a
GNANESHWAR <pappulagnaneshwarreddy@gmail.com>
2026-09-12 18:37:39 +0530

    Refactor keyboard input handling in keyboard.c

--- 2520030552_Skill/Skill - 2/keyboard.c
+++ 2520030552_Skill/Skill - 2/keyboard.c
@@ -2,39 +2,39 @@
 
 int main()
 {
-    char input[50];
-    char ch;
-    int i = 0;
+char input[50];
+char ch;
+int i = 0;
 
-    printf("Enter command: ");
+printf("Enter command: ");
 
-    while (1)
-    {
-        ch = getchar();
+while (1)
+{
+ch = getchar();
 
-        if (ch == '\n')
-        {
-            input[i] = '\0';
+if (ch == '\n')
+{
+input[i] = '\0';
 
-            printf("You entered: %s\n", input);
+printf("You entered: %s\n", input);
 
-            if (input[0] == 'e' && input[1] == 'x' &&
-                input[2] == 'i' && input[3] == 't')
-            {
-                break;
-            }
+if (input[0] == 'e' && input[1] == 'x' &&
+input[2] == 'i' && input[3] == 't')
+{
+break;
+}
 
-            i = 0;
-            printf("Enter command: ");
-        }
-        else
-        {
-            input[i] = ch;
-            i++;
-        }
-    }
+i = 0;
+printf("Enter command: ");
+}
+else
+{
+input[i] = ch;
+i++;
+}
+}
 
-    printf("Program ended\n");
+printf("Program ended\n");
 
-    return 0;
+return 0;
 }
