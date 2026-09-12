4ba6f55726b62adf55516d2c1ccbf78e8a067655
GNANESHWAR <pappulagnaneshwarreddy@gmail.com>
2026-09-12 18:45:30 +0530

    Add zombie.c to demonstrate process forking

---
+++ 2520030552_Practical/Practical - 4/zombie.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+int pid;
+pid=fork();
+if(pid==0)
+{
+printf("Child process %d\n",getpid());
+exit(0);
+}
+else {
+printf("Parent process %d \n",getpid());
+//sleep(30);
+wait(NULL);
+printf("Parent process completed");
+}
+}
