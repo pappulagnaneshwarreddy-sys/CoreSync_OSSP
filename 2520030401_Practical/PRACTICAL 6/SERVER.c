28a7426777110a43ff031a4e8b31bcd14cb2d8b4
Yashas Kasula <79995682+yashu-cyber@users.noreply.github.com>
2026-09-12 13:40:47 +0530

    Create SERVER.c

---
+++ 2520030401_Practical/PRACTICAL 6/SERVER.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main()
+{
+    char message[100];
+    char response[100];
+
+    mkfifo("myfifo", 0666);
+    mkfifo("responsefifo", 0666);
+
+    printf("Server started...\n");
+
+    while(1)
+    {
+        int fd = open("myfifo", O_RDONLY);
+
+        read(fd, message, sizeof(message));
+
+        close(fd);
+
+        printf("Client: %s", message);
+
+        printf("Processing message...\n");
+
+        int fd2 = open("responsefifo", O_WRONLY);
+
+        write(fd2, "Server processed: ", 18);
+        write(fd2, message, sizeof(message));
+
+        close(fd2);
+
+        printf("Response sent to Client\n");
+    }
+
+    return 0;
+}
