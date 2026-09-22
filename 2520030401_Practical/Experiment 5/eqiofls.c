023669696e841ca710302db98fc6cc894c064f62
Yashas Kasula <79995682+yashu-cyber@users.noreply.github.com>
2026-09-22 11:26:36 +0530

    Create eqiofls.c

---
+++ 2520030401_Practical/Experiment 5/eqiofls.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main()
+{
+int fd[2];
+int pid;
+pipe(fd);
+pid=fork();
+if(pid==0)
+{
+close(fd[0]);
+dup2(fd[1],STDOUT_FILENO);
+close(fd[1]);
+execlp("ls","ls","-l",NULL);
+}
+else
+{
+pid=fork();
+if(pid==0)
+{
+close(fd[1]);
+dup2(fd[0],STDIN_FILENO);
+close(fd[0]);
+execlp("grep","grep",".c",NULL);
+}
+else
+{
+close(fd[0]);
+close(fd[1]);
+wait(NULL);
+wait(NULL);
+}
+}
+return 0;
+}
